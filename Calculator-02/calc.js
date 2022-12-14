e3507df1d314d872842a6e5f965b79f03c62b986
Jabes Eduardo <kodaiwya@gmail.com>
2022-12-14 16:13:08 -0300

    Update Project

---
+++ Calculator-02/calc.js
@@ -0,0 +1,10 @@
+function sum( a = required(), b = required() ) { 
+    return a + b; 
+}
+
+function sub( a = required(), b = required() ) { 
+    return a - b; 
+}
+
+sum(console.log(7 + 7));
+sub(console.log(3 - 4));
